2778fe76e4a5ff39c9007212e7161894425f558e
hayamizhin <max9173627>
2020-01-24 12:07:21 +0900

    mysqlの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :high_scores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'high_scores#index'
 end
